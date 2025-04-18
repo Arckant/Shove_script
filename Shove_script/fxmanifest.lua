ee6af3391288c5e593d6aef30115bbb0e6499dab
Arckant <108737908+Arckant@users.noreply.github.com>
2025-04-18 16:46:00 +0200

    Update fxmanifest.lua

--- Shove_script/fxmanifest.lua
+++ Shove_script/fxmanifest.lua
@@ -1,10 +1,11 @@
 version '1.1.0'
 author 'arckant'
-repository 'https://github.com/Arckant/Shove_script'
+repository 'https://github.com/citizenfx/cfx-server-data'
 
 resource_type 'client_script' { name = 'Shove' }
 
 client_script 'client.lua'
+server_script 'server.lua'
 
 game 'gta5'
 fx_version 'cerulean'
