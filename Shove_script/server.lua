dd39560c58ff6e7eff3872159cc94544c49479f0
Arckant <108737908+Arckant@users.noreply.github.com>
2025-04-18 16:49:09 +0200

    Update server.lua

--- Shove_script/server.lua
+++ Shove_script/server.lua
@@ -1,7 +1,5 @@
 RegisterNetEvent('Shove', function(targetid, player, looking_vector)
-	--print(source)
 
-	--TriggerClientEvent('chat:addMessage', targetid, { args = {targetid, source}, color = 255,255,255 })
   TriggerClientEvent("ShoveImpact", targetid, looking_vector)
 
 end)
